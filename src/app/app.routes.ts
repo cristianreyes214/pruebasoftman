6e579e97ee259a6d6b960841db863b3c02c2eebd
cristianreyes214 <cristian.reyes@uao.edu.co>
2025-01-25 00:02:47 -0500

    Primera subida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', pathMatch: 'full', redirectTo: '/perfil'
+    },
+    {
+        path: 'perfil', component: ProfileComponent,
+    },
+];
---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+  email: string = '';
+  currentPassword: string = '';
+  newPassword: string = '';
+  confirmPassword: string = '';
+
+}
